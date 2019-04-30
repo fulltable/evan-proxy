9afd162bfc3c4e5542394f649ebedf62a0d1c4bd
Evan Kaplan <evankaplan@fb.com>
2019-04-30 16:03:23 -0700

    Reference server's elastic IPs

--- server/controllers.js
+++ server/controllers.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 
+const serverURL = `http://ec2-54-215-246-64.us-west-1.compute.amazonaws.com:3001`
+
 module.exports.reviewsPost = (req, res) => {
   const { restaurant_id } = req.params;
-  axios.post(`http://localhost:3001/api/restaurants/${restaurant_id}/reviews`, req.body)
+  axios.post(`${serverURL}/api/restaurants/${restaurant_id}/reviews`, req.body)
     .then(response => res.send(response.data))
     .catch(err => {
       console.error(err);
@@ -12,7 +14,7 @@ module.exports.reviewsPost = (req, res) => {
 
 module.exports.reviewsGet = (req, res) => {
   const { restaurant_id } = req.params;
-  axios.get(`http://localhost:3001/api/restaurants/${restaurant_id}/reviews`)
+  axios.get(`${serverURL}/api/restaurants/${restaurant_id}/reviews`)
     .then(response => res.send(response.data))
     .catch(err => {
       console.error(err);
@@ -23,7 +25,7 @@ module.exports.reviewsGet = (req, res) => {
 module.exports.reviewsPatch = (req, res) => {
   const { restaurant_id } = req.params;
   const { review_id } = req.params;
-  axios.patch(`http://localhost:3001/api/restaurants/${restaurant_id}/reviews/${review_id}`, req.body)
+  axios.patch(`${serverURL}/api/restaurants/${restaurant_id}/reviews/${review_id}`, req.body)
     .then(response => res.send(response.data))
     .catch(err => {
       console.error(err);
@@ -34,7 +36,7 @@ module.exports.reviewsPatch = (req, res) => {
 module.exports.reviewsDelete = (req, res) => {
   const { restaurant_id } = req.params;
   const { review_id } = req.params;
-  axios.delete(`http://localhost:3001/api/restaurants/${restaurant_id}/reviews/${review_id}`)
+  axios.delete(`${serverURL}/api/restaurants/${restaurant_id}/reviews/${review_id}`)
     .then(response => res.send(response.data))
     .catch(err => {
       console.error(err);
